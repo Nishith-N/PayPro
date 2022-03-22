f57409611814609e7016c6ffba427cb8bb85df2a
KowshikHarish <kowshikharish@gmail.com>
2022-03-22 22:40:38 +0530

    Transaction

---
+++ Transaction/error.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+    <link href="error_style.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <center>
+    <table style="padding-top: 250px;">
+        <tr>
+            <td>
+                <div class="error alert">
+                    <div class="alert-body">
+                      Error !
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td style="padding-left: 90px;padding-top: 20px;">
+                <form method="post" action="error.html">
+                    <input type="submit"  value="Go To Home" id="gotohome_btn" name="gotohome_btn" class="home" >
+                </form>
+            </td>
+        </tr>
+    </table>
+</center>     
+</body>
+</html>
---
+++ Transaction/success.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="success_style.css" rel="stylesheet">
+
+    <title>Success</title>
+</head>
+<body>  
+  <br>
+
+    <div class="wrapperAlert">
+        <div class="contentAlert">      
+          <div class="topHalf">      
+            <p><svg viewBox="0 0 512 512" width="100" title="check-circle">
+              <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" />
+              </svg></p>
+            <h1>Congratulations,</h1>
+            <br>
+            <h1>Transaction Successful</h1>           
+            <br>
+           <ul class="bg-bubbles">
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+           </ul>
+          </div>
+        </div>                
+      </div>
+      <form method="post" action="error.php" style="margin-top: 500px;margin-left: -300px;">
+        <input type="submit"  value="Go To Home" id="gotohome_btn" name="gotohome_btn" class="home" >
+      </form>    
+</body>
+</html>
