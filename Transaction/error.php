d26548d27264718676cc81764184ffa7ff907165
KowshikHarish <kowshikharish@gmail.com>
2022-03-23 18:40:28 +0530

    transaction

--- Transaction/error.php
+++ Transaction/error.php
@@ -4,44 +4,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error</title>
+    <title>Document</title>
     <link href="error_style.css" rel="stylesheet">
+    <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
-<header id="header" style="margin-top: -22px;" class="fixed-top ">
+    <header id="header" style="margin-top: -22px;" class="fixed-top ">
         <div class="container d-flex align-items-center">  
           <section>
             <span class="full-text" >
               <nav id="navbar" class="navbar" style="margin-top: 0%;">
-                
-              </nav>
-              <br>
-              <!-- .navbar --> 
-            </span>
-
-            <span class="short-text">
-              <nav id="navbar" class="navbar">
                 <ul>
-                <?php print '<li><h1 class="logo me-auto" ><a href=""><i style="font-size: 35px;"><strong>PayPro,Hi '.$username.'</strong></i></a></h1></li>'?>
-                  
+                    <br>
+                  <li><h1 class="logo me-auto" ><a href=""><i style="font-size: 35px;margin-top: 20px;"><strong>PayPro</strong></i></a></h1></li>                  
                 </ul>
               </nav>
               <br>
-              <nav id="navbar" class="navbar">
-              
-              </nav>
               <!-- .navbar --> 
             </span>
           </section>
         </div>
       </header>
-      <!-- End Header -->
     <center>
-    <table style="padding-top: 250px;">
+    <table style="padding-top: 150px;">
         <tr>
             <td>
                 <div class="error alert">
@@ -59,6 +66,13 @@
             </td>
         </tr>
     </table>
-</center>     
+</center>
+    
+
+    
+    
+
+    
+    
 </body>
 </html>
--- Transaction/success.php
+++ Transaction/success.php
@@ -6,39 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="success_style.css" rel="stylesheet">
+    <link rel="icon" href="https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/46/d1/61/46d16165-c305-5c6f-7626-1a60208042f3/source/512x512bb.jpg" type="image/icon type">
 
     <title>Success</title>
 </head>
-<body>  
-  <br>
 
+<body>  
+  <nav id="navbar" class="navbar" style="margin-top: 0%;">                
+    <h1 class="logo me-auto" ><a href="" style="text-decoration: none;padding-bottom: 10px;"><i style="font-size: 35px;margin-top: 20px;"><strong>PAYPRO</strong></i></a></h1>
+  </nav>
+  
+  <br>
+  <div class="body1">
     <div class="wrapperAlert">
-        <div class="contentAlert">      
-          <div class="topHalf">      
-            <p><svg viewBox="0 0 512 512" width="100" title="check-circle">
-              <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" />
-              </svg></p>
-            <h1>Congratulations,</h1>
-            <br>
-            <h1>Transaction Successful</h1>           
-            <br>
-           <ul class="bg-bubbles">
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-             <li></li>
-           </ul>
-          </div>
-        </div>                
-      </div>
+      <div class="contentAlert">      
+        <div class="topHalf">      
+          <p><svg viewBox="0 0 512 512" width="100" title="check-circle">
+            <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" />
+            </svg></p>
+          <h1>Congratulations,</h1>
+          <br>
+          <h1>Transaction Successful</h1>           
+          <br>
+         <ul class="bg-bubbles">
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+         </ul>
+        </div>
+      </div>                
+    </div>
+    
       <form method="post" action="error.php" style="margin-top: 500px;margin-left: -300px;">
         <input type="submit"  value="Go To Home" id="gotohome_btn" name="gotohome_btn" class="home" >
-      </form>    
+      </form> 
+    </div>   
 </body>
 </html>
