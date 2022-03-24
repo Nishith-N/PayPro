db0d564a76a880510cefcc4209c0711b5727ea40
KowshikHarish <kowshikharish@gmail.com>
2022-03-24 12:25:43 +0530

    navbar

--- Profile/manageprofile.php
+++ Profile/manageprofile.php
@@ -128,6 +128,49 @@ $ifname=$_POST['fname'];
 	<link rel="stylesheet" type="text/css" href="manageprofile_style.css">   
 </head>
 <body>
+<header id="header" style="margin-top: -22px;" class="fixed-top ">
+        <div class="container d-flex align-items-center">  
+          <section>
+            <span class="full-text" >
+              <nav id="navbar" class="navbar" style="margin-top: 0%;">
+                <ul>
+                  <li><h1 class="logo me-auto" ><a href=""><i style="font-size: 35px;"><strong>PayPro</strong></i></a></h1></li>
+                  <ul style="margin-left: 29%;">
+                    <li><a class="nav-link scrollto" href="../Profile/manageprofile.php">Manage Profile</a></li>
+                    <li><a class="nav-link scrollto" href="../Userhome/wallet_recharge.php">Recharge</a></li>
+                    <li><a class="nav-link scrollto" href="../Userhome/wallet_remove.php">Remove Money</a></li>
+                    <li><a class="nav-link scrollto" href="../Profile/reset_password.php">Reset Password</a></li>
+                    <li><a class="getstarted scrollto" href="../Login/login.php">SignOut  </a></li>
+                  </ul>
+                </ul>
+              </nav>
+              <br>
+              <!-- .navbar --> 
+            </span>
+
+            <span class="short-text">
+              <nav id="navbar" class="navbar">
+                <ul>
+                  <li><h1 class="logo me-auto" ><a href=""><i style="font-size: 35px;"><strong>PayPro</strong></i></a></h1></li>
+                  
+                </ul>
+              </nav>
+              <br>
+              <nav id="navbar" class="navbar">
+              <ul style="margin-left: 0%;">
+              <li><a class="nav-link scrollto" href="../Profile/manageprofile.php">Manage Profile</a></li>
+                    <li><a class="nav-link scrollto" href="../Userhome/wallet_recharge.php">Recharge</a></li>
+                    <li><a class="nav-link scrollto" href="../Userhome/wallet_remove.php">Remove Money</a></li>
+                    <li><a class="nav-link scrollto" href="../Profile/reset_password.php">Reset Password</a></li>
+                    <li><a class="getstarted scrollto" href="../Login/login.php">SignOut  </a></li>
+              </ul>
+              </nav>
+              <!-- .navbar --> 
+            </span>
+          </section>
+        </div>
+      </header>
+      <!-- End Header -->
     
 <div class="container">
     
