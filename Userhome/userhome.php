69299dc92328a0cd70220b37ef887cde86d9d90c
shree-29 <kkshree2910@gmail.com>
2022-03-24 23:46:09 +0530

    sign out

--- Userhome/userhome.php
+++ Userhome/userhome.php
@@ -79,7 +79,13 @@ if(mysqli_num_rows($result)==1)
                     <li><a class="nav-link scrollto" href="../Transaction/form.php">Transaction</a></li>
                     <li><a class="nav-link scrollto" href="../Contacts/list_contact.php">Contact</a></li>
                     <li><a class="nav-link scrollto" href="../Profile/profilehome.php">Profile</a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <li><form method="post" action="#"><input type="submit"  value="SignOut" id="signout" name="signout" class="signout_btn"></form></li>
+  <li><form method="post" action="../Login/login.php"><input type="submit"  style=" width: 120px;
+        border-radius: 20px;
+        height: 40px;
+        border-color: white;
+        background-color: #892883;
+        color: white;
+        font-size: 18px;" value="SignOut" id="signout" name="signout" class="signout_btn"></form></li>
                   </ul>
                 </ul>
               </nav>
@@ -102,7 +108,13 @@ if(mysqli_num_rows($result)==1)
                     <li><a class="nav-link scrollto" href="../Transaction/form.php">Transaction</a></li>
                     <li><a class="nav-link scrollto" href="../Contacts/list_contact.php">Contact</a></li>
                     <li><a class="nav-link scrollto" href="../Profile/profilehome.php">Profile</a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <li><form method="post" action="../Login/login.php"><input type="submit"  value="SignOut" id="signout" name="signout" class="signout_btn"></form></li>
+  <li><form method="post" action="../Login/login.php"><input type="submit" style=" width: 120px;
+        border-radius: 20px;
+        height: 40px;
+        border-color: white;
+        background-color: #892883;
+        color: white;
+        font-size: 18px;" value="SignOut" id="signout" name="signout" class="signout_btn"></form></li>
               </ul>
               </nav>
               <!-- .navbar --> 
