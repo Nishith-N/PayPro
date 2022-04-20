91476dc71353d8a78b61da8da70b2f29b77b8f35
KowshikHarish <kowshikharish@gmail.com>
2022-04-21 01:01:30 +0530

    search transaction

--- Transaction/search_transaction.php
+++ Transaction/search_transaction.php
@@ -102,10 +102,10 @@ if(isset($_POST['signout']))
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Transaction</title>
+	<title>Search Transaction</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="pay_style.css">   
+	<link rel="stylesheet" type="text/css" href="search_transaction_style.css">   
 	<link rel="icon" href="https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/46/d1/61/46d16165-c305-5c6f-7626-1a60208042f3/source/512x512bb.jpg" type="image/icon type">
 </head>
 <body>
@@ -173,25 +173,144 @@ if(isset($_POST['signout']))
 		<div class="cardregister" style="margin-top: 180px;">
            
 			<div class="card-header">
-				<h3 class="register">Transaction</h3>				
+				<h3 class="register">Search Transaction</h3>				
 			</div>
 
 			<div class="card-body" >
 				<form action="#" method="post">
 
                     <div class="form-group">
-                        <label for="phno" class="textregister">Phone Number </label><label for="phno" class="starregister"> * </label>
-                        <input type="number" id="phno" name="phno" class="form-control" style="width: 300px;">
+                        <label for="cno" class="textregister">Card Number </label><label for="cno" class="starregister"> * </label>
+                        <input type="text" id="cno" name="cno" class="form-control" style="width: 300px;">
                     </div> 
 
-                    <div class="form-group">
-                        <label for="amount" class="textregister">Amount </label><label for="amount" class="starregister"> * </label>
-                        <input type="number" id="amount" name="amount" class="form-control" style="width: 300px;"> 
-                    </div>                  
+                    <label for="date" class="textregister">From</label><label for="date" class="starregister"> * </label>
+                    <div class="form-row">
+                        <div class="form-group col-md-3">
+                          <label for="date" class="textregister">Date</label>
+                          <select id="date" class="form-control">
+                            <option selected value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>                            
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                          </select>
+                        </div>
+
+                        <div class="form-group col-md-3">
+                          <label for="month" class="textregister">Month</label>
+                          <select id="month" class="form-control">
+                            <option selected value="Jan">Jan</option>
+                            <option value="Feb">Feb</option>
+                            <option value="Mar">Mar</option>
+                            <option value="Apr">Apr</option>
+                            <option value="May">May</option>
+                            <option value="Jun">Jun</option>
+                            <option value="Jul">Jul</option>
+                            <option value="Aug">Aug</option>
+                            <option value="Sep">Sep</option>
+                            <option value="Oct">Oct</option>
+                            <option value="Nov">Nov</option>
+                            <option value="Dec">Dec</option>
+                          </select>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                          <label for="zip" class="textregister">Year</label>
+                          <input type="text" class="form-control" id="zip" name="zip">
+                        </div>
+                    </div>      
+                    <label for="date" class="textregister">To</label><label for="date" class="starregister"> * </label>
+                    <div class="form-row">                        
+                        <div class="form-group col-md-3">
+                          <label for="date" class="textregister">Date</label>
+                          <select id="date" class="form-control">
+                            <option selected value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>                            
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                          </select>
+                        </div>
+
+                        <div class="form-group col-md-3">
+                          <label for="month" class="textregister">Month</label>
+                          <select id="month" class="form-control">
+                            <option selected value="Jan">Jan</option>
+                            <option value="Feb">Feb</option>
+                            <option value="Mar">Mar</option>
+                            <option value="Apr">Apr</option>
+                            <option value="May">May</option>
+                            <option value="Jun">Jun</option>
+                            <option value="Jul">Jul</option>
+                            <option value="Aug">Aug</option>
+                            <option value="Sep">Sep</option>
+                            <option value="Oct">Oct</option>
+                            <option value="Nov">Nov</option>
+                            <option value="Dec">Dec</option>
+                          </select>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                          <label for="zip" class="textregister">Year</label>
+                          <input type="text" class="form-control" id="zip" name="zip">
+                        </div>
+                    </div>
 
 					<div class="form-group">
-						<input type="submit" value="Submit" class="btn float-right login_btn" name="submit_btn" id="submit_btn"><br><br>
-						
+						<input type="submit" value="Search" class="btn float-right login_btn" name="submit_btn" id="submit_btn"><br><br>						
 					</div>
 				</form>
 			</div>
