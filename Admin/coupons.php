5fa6d0d5a2b73edbdb2a05d6b2db55cebc8bac21
N Nishith <nishithnkutty@gmail.com>
2022-04-28 10:13:25 +0530

    kjnksn

--- Admin/coupons.php
+++ Admin/coupons.php
@@ -74,8 +74,8 @@
 			<div class="card-body" >
 				<form method="post" action="#">
                     <div class="form-group">
-                        <label for="card_num" class="textaddcontact">Card Number</label><label for="card_num" class="starregister"> * </label>
-                        <input type="number" id="card_num" name="card_num" class="form-control" style="width: 300px;">
+                        <label for="card_num" class="textaddcontact">Phone Number/Email</label><label for="card_num" class="starregister"> * </label>
+                        <input type="text" id="uname" name="uname" class="form-control" style="width: 300px;">
                     </div>  
                     
                     <div class="form-group">
