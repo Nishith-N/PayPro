040616ad08b3ce14809926304621c998408a71e7
KowshikHarish <kowshikharish@gmail.com>
2022-04-28 12:26:30 +0530

    export

--- Admin/export.php
+++ Admin/export.php
@@ -29,36 +29,69 @@ if(isset($_POST["export_data"])) {
 
 ?>
 
-
-<html>
-<div class="container">	
-	<h2>Export Data to Excel</h2>
-	<div >
-		<div>	
-			<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">					
-				<button type="submit" id="export_data" name='export_data' value="Export to excel">Export to excel</button>
-			</form>
-		</div>
-	</div>				  
-	<table border="1">
-		<tr>
-			<th>Payer</th>
-			<th>Payee</th>
-			<th>Amount</th>	
-			<th>Date</th>
-			<th>Reason</th>
-		</tr>
-		<tbody>
-			<?php foreach($developer_records as $developer) { ?>
-			   <tr>
-			   <td><?php echo $developer ['pay_username']; ?></td>
-			   <td><?php echo $developer ['pay_name']; ?></td>
-			   <td><?php echo $developer ['amount']; ?></td>   
-			   <td><?php echo $developer ['dates']; ?></td>
-			   <td><?php echo $developer ['reasons']; ?></td>   
-			   </tr>
-			<?php } ?>
-		</tbody>
-    </table>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="export_style.css" rel="stylesheet">
+</head>
+<body>
+    <br><br>
+    <center>
+    <div class="container">	
+        <div><img src="https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/46/d1/61/46d16165-c305-5c6f-7626-1a60208042f3/source/512x512bb.jpg" class="logo"></div>
+        <h1>Transaction Details</h1>
+        <div >
+            <div>	
+                <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">					
+                    <button type="submit" id="export_data" name='export_data' value="Export to excel">Export to excel</button>
+                </form>
+            </div>
+        </div>				  
+        <table border="1" class="styled-table">
+            <thead>
+            <tr>
+                <th>Payer</th>
+                <th>Payee</th>
+                <th>Amount</th>	
+                <th>Date</th>
+                <th>Reason</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php foreach($developer_records as $developer) { ?>
+                    <tr>
+                    <td><?php echo $developer ['pay_username']; ?></td>
+                    <td><?php echo $developer ['pay_name']; ?></td>
+                    <td><?php echo $developer ['amount']; ?></td>   
+                    <td><?php echo $developer ['dates']; ?></td>
+                    <td><?php echo $developer ['reasons']; ?></td>   
+                    </tr>
+                 <?php } ?>
+                 <?php foreach($developer_records as $developer) { ?>
+                    <tr>
+                    <td><?php echo $developer ['pay_username']; ?></td>
+                    <td><?php echo $developer ['pay_name']; ?></td>
+                    <td><?php echo $developer ['amount']; ?></td>   
+                    <td><?php echo $developer ['dates']; ?></td>
+                    <td><?php echo $developer ['reasons']; ?></td>   
+                    </tr>
+                 <?php } ?>
+                 <?php foreach($developer_records as $developer) { ?>
+                    <tr>
+                    <td><?php echo $developer ['pay_username']; ?></td>
+                    <td><?php echo $developer ['pay_name']; ?></td>
+                    <td><?php echo $developer ['amount']; ?></td>   
+                    <td><?php echo $developer ['dates']; ?></td>
+                    <td><?php echo $developer ['reasons']; ?></td>   
+                    </tr>
+                 <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</center>
+</body>
 </html>
