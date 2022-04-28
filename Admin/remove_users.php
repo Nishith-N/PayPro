3c0afbf8c95763dd0493cf2bd0ebec465e86f7e4
N Nishith <nishithnkutty@gmail.com>
2022-04-28 09:40:08 +0530

    remove user

--- Admin/remove_users.php
+++ Admin/remove_users.php
@@ -1,3 +1,43 @@
+<?php
+$db = mysqli_connect('localhost','root','','payprodb')
+or die('Error connecting to MySQL server.');
+		session_start();
+
+    if(isset($_POST['submit_btn']))
+    {
+      $uname=$_POST['phno'];
+
+     
+      $sql="SELECT pay_id FROM user_details WHERE phone='".$uname."' OR email='".$uname."'";
+		  $result=mysqli_query($db,$sql);
+      $row=mysqli_fetch_row($result);
+      $payid=$row[0];
+      $sql="DELETE FROM user_details WHERE phone='".$uname."' OR email='".$uname."'";
+		  $result=mysqli_query($db,$sql);
+      $num1=mysqli_num_rows($result);
+
+      $sql="DELETE FROM contact_details WHERE username='".$uname."' AND pay_id='".$payid."'";
+		  $result=mysqli_query($db,$sql);
+      $num2=mysqli_num_rows($result);
+
+      $sql="DELETE FROM card_details WHERE pay_id='".$payid."'";
+		  $result=mysqli_query($db,$sql);
+      $num3=mysqli_num_rows($result);
+
+      if($num1!=0)
+      {
+        header("Location:../Admin/adminlogin.php");
+        exit();
+      }
+      else{
+
+      }
+    }
+
+
+?>
+
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -74,8 +114,8 @@
 			<div class="card-body" >
 				<form method="post" action="#">
                     <div class="form-group">
-                        <label for="phno" class="textaddcontact">Phone Number</label><label for="phno" class="starregister"> * </label>
-                        <input type="number" id="phno" name="phno" class="form-control" style="width: 300px;">
+                        <label for="phno" class="textaddcontact">Phone Number/Email</label><label for="phno" class="starregister"> * </label>
+                        <input type="text" id="phno" name="phno" class="form-control" style="width: 300px;">
                     </div>                   
 
 					<div class="form-group">
